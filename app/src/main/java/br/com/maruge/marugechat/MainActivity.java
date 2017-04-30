4bd94846e2b57d8fd6c119a3afb7b3c8cb630daf
Nayana21 <david@maruge.com.br>
2017-04-30 02:27:04 -0300

    Criado a tela e a classe TelaNovoUsuário

--- app/src/main/java/br/com/maruge/marugechat/MainActivity.java
+++ app/src/main/java/br/com/maruge/marugechat/MainActivity.java
@@ -1,13 +1,39 @@
 package br.com.maruge.marugechat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    EditText EdtUsuario,EdtSenha;
+    Button btnlogar;
+    TextView txtNovoUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EdtUsuario = (EditText)findViewById(R.id.EdtUsuario);
+        EdtSenha = (EditText)findViewById(R.id.EdtSenha);
+        btnlogar = (Button)findViewById(R.id.btnlogar);
+        txtNovoUsuario = (TextView)findViewById(R.id.txtNovoUsuario);
+
+        txtNovoUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent novoUsuario = new Intent(MainActivity.this, TelaNovoUsuario.class);
+                startActivity(novoUsuario);
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/br/com/maruge/marugechat/TelaNovoUsuario.java
@@ -0,0 +1,22 @@
+package br.com.maruge.marugechat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jefferson David on 30/04/2017.
+ */
+
+public class TelaNovoUsuario extends AppCompatActivity {
+
+
+    @Override
+    protected  void onCreate(Bundle SavedInstanceState){
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+    }
+
+
+
+
+}
